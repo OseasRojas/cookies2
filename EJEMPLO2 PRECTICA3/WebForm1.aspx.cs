dc2d9fd888c89cc6233d7f95997ccb8dbd3750e7
Oseas <47002@Oseas.com>
2025-03-06 10:53:17 -0600

    primer commit ej2

---
+++ EJEMPLO2 PRECTICA3/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cookies2_4700237
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            Response.Redirect
+                (
+                "WebForm2.aspx" +
+                "?ddlCategory=" + ddlCategory.SelectedValue +
+                "&ddlSupplier=" + ddlSupplier.SelectedValue +
+                "&strProduct=" + strProduct.Text +
+                "&strDescription=" + strDescription.Text +
+                "&strImage=" + strImage.Text +
+                "&decPrice=" + decPrice.Text +
+                "&bytNumberInStock=" + bytNumberInStock.Text +
+                "&bytNumberOnOrder=" + bytNumberOnOrder.Text +
+                "&bytReorderLevel=" + bytReorderLevel.Text);
+        }
+    }
+}
